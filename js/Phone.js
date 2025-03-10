053a07bc43b89adb0605d3829873c5d2bbcedc6e
mdalrasel <mdalrasel.dev@gmail.com>
2025-03-11 05:00:16 +0600

    compleated this repo

--- js/Phone.js
+++ js/Phone.js
@@ -1,26 +1,29 @@
 
-const loadPhone = async (searchText) => {
+const loadPhone = async (searchText='f',isShowAll) => {
     const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
     const data = await res.json();
     const phones = data.data;
-    displayPhones(phones)
+    displayPhones(phones,isShowAll)
 
 }
 
 
-const displayPhones = phones => {
+
+const displayPhones = (phones,isShowAll) => {
     const phoneContainer = document.getElementById('phone-container');
     phoneContainer.textContent = '';
 
     const showAllContainerBtn = document.getElementById('showAllContainer');
-    if (phones.length > 9) {
+    if (phones.length > 9 && !isShowAll) {
         showAllContainerBtn.classList.remove('hidden')
     }
     else {
         showAllContainerBtn.classList.add('hidden')
     }
 
-    phones = phones.slice(0, 9)
+    if(!isShowAll){
+        phones = phones.slice(0, 9)
+    }
 
 
     phones.forEach(phone => {
@@ -36,7 +39,7 @@ const displayPhones = phones => {
             <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
             <p class="font-bold text-2xl">$999</p>
             <div class="card-actions">
-                <button class="text-white  font-bold bg-[#0D6EFD] px-4 py-2 rounded-lg cursor-pointer">Show Details</button>
+                <button onclick="handleShowDetail('${phone.slug}') " class="text-white  font-bold bg-[#0D6EFD] px-4 py-2 rounded-lg cursor-pointer">Show Details</button>
             </div>
         </div>`;
         phoneContainer.appendChild(phoneCard)
@@ -44,11 +47,58 @@ const displayPhones = phones => {
     toggleLoadingSpinner(false)
 }
 
-const handleSearch = () => {
+const handleShowDetail =async(id)=>{
+    const res = await fetch(` https://openapi.programming-hero.com/api/phone/${id}`);
+    const data = await res.json();
+    const phone = data.data;
+    phoneShowDetails(phone)
+}
+
+const phoneShowDetails = (phone)=>{
+    const showDetailsContainer= document.getElementById('show-details-container');
+    showDetailsContainer.innerHTML=`
+    <div class="bg-white p-6 rounded-lg shadow-lg w-96">
+            <div class="flex justify-center">
+                <img src="${phone.image}" alt="Product Image" class="rounded-lg">
+            </div>
+            <h2 class="text-xl font-semibold mt-4">${phone.name
+            }</h2>
+            <p class="text-gray-500 text-sm mt-1">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+
+            <div class="mt-4 space-y-2">
+                <p><strong>Storage:</strong>${phone?.mainFeatures?.storage
+                }</p>
+                <p><strong>Display Size:</strong>${phone?.mainFeatures?.displaySize
+                } </p>
+                <p><strong>Chipset:</strong> ${phone?.mainFeatures?.chipSet
+                }</p>
+                <p><strong>Memory:</strong>  ${phone?.mainFeatures?.memory
+                }</p>
+                <p><strong>Slug:</strong> ${phone?.slug
+                } </p>
+                <p><strong>Release Date:</strong>${phone?.releaseDate}</p>
+                <p><strong>Brand:</strong> ${phone?.brand}</p>
+                <p><strong>GPS:</strong> ${phone?.others?.GPS
+                }</p>
+            </div>
+
+            <!-- Close Button -->
+            <div class="modal-action">
+                <form method="dialog">
+                    <button class="btn">Close</button>
+                </form>
+            </div>
+    </div>
+    
+    `
+    show_details_modal.showModal()
+}
+
+const handleSearch = (isShowAll) => {
     toggleLoadingSpinner(true)
     const searchFiled = document.getElementById('search-filed');
     const searchText = searchFiled.value;
-    loadPhone(searchText)
+    loadPhone(searchText,isShowAll)
 }
 
 const toggleLoadingSpinner = (isLoading) => {
@@ -60,3 +110,10 @@ const toggleLoadingSpinner = (isLoading) => {
         loadingSpinner.classList.add('hidden')
     }
 }
+
+
+const handleShowAll = ()=>{
+    handleSearch(true)
+}
+
+loadPhone()
